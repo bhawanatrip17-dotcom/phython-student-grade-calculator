b20142d38b8dbd465632f0161e7c7c377991071b
bhawanatrip17-dotcom <bhawanatrip17@gmail.com>
2026-08-16 16:27:45 +0530

    Add files via upload

---
+++ Student grade calculator.py
@@ -0,0 +1,10 @@
+per=float(input("Enter your percentage:"))
+if per>=80:
+    print("your grade is A")
+elif 60<=per<80:
+    print("your grade is B")
+elif 40<=per<60:
+    print("your grade is C")
+elif per<40:
+    print("your grade is D")
+    
